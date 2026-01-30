b95ea5151c17db8df98ebeae370c58a04aa2028f
Arthit Thongpaiboon <attpb04@gmail.com>
2026-01-30 14:17:52 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/Card1.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+
+}
+class _HomeState extends State<Home> {
+
+  int _selectedIndex = 0;
+  static List<Widget> pages = <Widget>[
+    const Card1(),
+    Container(color: Colors.red),
+    Container(color: Colors.green)
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+      body: Center(child: pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 1',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 2',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card 3',
+          ),
+        ],
+      ),
+    );
+  }
+}
